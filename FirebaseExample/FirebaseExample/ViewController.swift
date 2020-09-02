4cd8fc93ebf7a5e34cba6afcff811d7b8cad0ffc
romankolossov <rkolossov@gmail.com>
2020-09-02 10:43:56 +0300

    firebase app prerations complete

---
+++ FirebaseExample/FirebaseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseExample
+//
+//  Created by Роман Колосов on 01.09.2020.
+//  Copyright © 2020 Roman N. Kolosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
